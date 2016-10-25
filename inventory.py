b181ac6f21c9a814132d2347e2cf3a6b2ff43038
Your Name <you@example.com>
2016-10-25 00:17:02 -0400

    Changed formatting to match pep8 standards

--- inventory.py
+++ inventory.py
@@ -1,43 +1,49 @@
-import requests, json, math
+import requests
+import json
+import math
 from flask import Flask, render_template, Markup, request
 import time
 
+
 with open('../steam_api_key') as f:
     apikey = f.read().strip()
-      
+
 
 app = Flask(__name__)
 
+
 @app.route('/displayInventory', methods=["POST"])
 def displayInventory():
-    userinput  = request.form['input']
-    steamid    = request.form['id']
-    itemid     = request.form['itemid']
+    userinput = request.form['input']
+    steamid = request.form['id']
+    itemid = request.form['itemid']
 
     response = main(convertID(str(steamid)))
     return response
-    
+
+
 def convertID(steamid):
     if not steamid.isdigit():
-        url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + apikey + "&vanityurl=" + steamid
+        url = ("http://api.steampowered.com/ISteamUser/ResolveVanityURL/" +
+               "v0001/?key=" + apikey + "&vanityurl=" + steamid)
         response = requests.get(url)
         steamid = json.loads(response.text)['response']['steamid']
     return steamid
-        
+
+
 @app.route('/')
 def home():
     return render_template('index.html', info_location="")
 
+
 def main(steamid):
     timeStart = time.time()
-    
-    #steamid = str(76561198261551396)    # mattrb
-    #steamid = str(76561197972331023)    # lololguardian
 
     with open('../steam_api_key') as f:
         apikey = f.read().strip()
 
-    url = "http://api.steampowered.com/IEconItems_730/GetPlayerItems/v0001/?key=" + apikey + "&SteamID=" + steamid
+    url = ("http://api.steampowered.com/IEconItems_730/GetPlayerItems/" +
+           "v0001/?key=" + apikey + "&SteamID=" + steamid)
 
     steamAPIcalls = 0
     while True:
@@ -57,24 +63,24 @@ def main(steamid):
 
     with open('json/patternDB.json') as f:
         patternDB = json.load(f, encoding='utf-8')
-    
+
     skins = []
-    keys  = {}
+    keys = {}
 
     for item in playerItems["result"]["items"]:
-        itemID   = item["id"]
+        itemID = item["id"]
         itemType = item["defindex"]
         try:
             itemName = itemDB["item"][str(itemType)]
         except:
-            itemName = "graffiti" ### pass ###
-            
+            itemName = "graffiti"  # pass
+
         if itemType <= 516 and item["attributes"][0]["defindex"] == 6:
             patternIndex = item["attributes"][0]["float_value"]
-            pattern      = skinDB["skin"][str(patternIndex)]
-            paintIndex   = str(int(item["attributes"][1]["float_value"]))
-            floatValue   = item["attributes"][2]["float_value"]
-            
+            pattern = skinDB["skin"][str(patternIndex)]
+            paintIndex = str(int(item["attributes"][1]["float_value"]))
+            floatValue = item["attributes"][2]["float_value"]
+
             special = ""
 
             if pattern == "Fade":
@@ -108,18 +114,20 @@ def main(steamid):
                     special = "Phase 4"
                 elif patternIndex == 568:
                     special = "Emerald"
-                    
+
             for attribute in item["attributes"]:
-              if (attribute["defindex"] == 81):
-                itemName = "<span class=\"text-pop\">StatTrak</span> " +  itemName;
-                break;
+                if attribute["defindex"] == 81:
+                    itemName = ("<span class=\"pop\">StatTrak</span> " +
+                                itemName)
+                    break
 
             if itemType >= 500:
-                itemName = "<span class=\"text-pop\">" + u'\u2605 ' + "</span>" + itemName
+                itemName = ("<span class=\"pop\">" + u'\u2605 ' +
+                            "</span>" + itemName)
 
             skin = [itemName, pattern, special, floatValue]
-
             skins.append(skin)
+
         elif "Key" in itemName:
             if itemName in keys:
                 keys[itemName] += 1
@@ -127,53 +135,59 @@ def main(steamid):
                 keys[itemName] = 1
 
     # timeTotal = time.time() - timeStart
-    # loadTimeHTML = "<div style=\"text-align:center\">" + "Loaded in " + str(round(timeTotal,2)) + " seconds" + "</div>"
-    
-    #output = Markup("<table>" + convertSkinString(skins) + convertKeyString(keys) + "</table>" + loadTimeHTML)
-    # return render_template('index.html', info_location=output), 200
 
-    return ("<table>" + convertSkinString(skins) + convertKeyString(keys) + "</table>")
-    
-def convertSkinString(skns):
+    return ("<table>" + skinString(skins) + keyString(keys) + "</table>")
+
+
+def skinString(skins):
     skinString = ""
 
-    if len(skns) != 0:
+    if len(skins) != 0:
         skinString += "<tr><th>Skins</th>"
-        skinString += ("<th>" + str(len(skns)) + "</th></tr>")
-        skinString += outputWeapon(skns)
+        skinString += ("<th>" + str(len(skins)) + "</th></tr>")
+        skinString += outputWeapon(skins)
 
     return skinString
+
 
 def outputWeapon(weapons):
     weapons = sortFloat(weapons)
     weaponString = ""
 
     for weapon in weapons:
-        weaponString += ("<tr><td>" + weapon[0] + " | " + weapon[1] + "<sup><span style=\"font-size:12px\" class=\"text-pop\"> " + weapon[2] + "</span></sup></td><td>" + "{0:.12f}".format(weapon[3]) + "</td></tr>")
+        weaponString += ("<tr><td>" + weapon[0] + " | " + weapon[1] +
+                         "<sup><span style=\"font-size:12px\" class=\"pop\">" +
+                         weapon[2] + "</span></sup></td>" +
+                         "<td>" + "{0:.12f}".format(weapon[3]) + "</td></tr>")
 
     return weaponString
 
+
 def sortFloat(skns):
     floats = [skn[3] for skn in skns]
-    return [s for (f,s) in sorted(zip(floats,skns))]
+    return [s for (f, s) in sorted(zip(floats, skns))]
 
-def convertKeyString(keys):
+
+def keyString(keys):
     keyList = sortKeys(keys)
     keyString = ""
 
     if len(keyList) != 0:
-        keyString += "<tr><th>Keys</th>"
-        keyString += ("<th>" + str(sum([keys[k] for k in keys])) + "</th></tr>")
+        keyString += ("<tr><th>Keys</th><th>" +
+                      str(sum([keys[k] for k in keys])) +
+                      "</th></tr>")
 
         for key in keyList:
-            keyString += ("<tr><td>" + key[0] + "</td><td>" + str(key[1]) + "</td></tr>")
+            keyString += ("<tr><td>" + key[0] + "</td>" +
+                          "<td>" + str(key[1]) + "</td></tr>")
 
     return keyString
 
+
 def sortKeys(keys):
     keyList = [[key, keys[key]] for key in keys]
-    counts = [c for (n,c) in keyList]
-    return [k for (c,k) in sorted(zip(counts,keyList))[::-1]]
-    
+    counts = [c for (n, c) in keyList]
+    return [k for (c, k) in sorted(zip(counts, keyList))[::-1]]
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='80', threaded=True)
