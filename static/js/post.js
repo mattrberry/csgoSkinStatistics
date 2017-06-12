2d62832973e4a1ed6a349a88fa47bfc0bd9c55a0
Matthew Berry <me@mattrb.com>
2017-06-12 13:25:46 -0400

    first start to a fix after valve broke the old api

--- static/js/post.js
+++ static/js/post.js
@@ -1,69 +1,56 @@
 $(document).ready(function() {
     $("#textbox").keydown(function(e){
-	if(e.which === 13){
+        if(e.which === 13){
             $("#button").focus();
-	}
+        }
     });
-    
+
     $("#button").click(function() {
-	$(this).blur();
-	
-	$("#display").animate({
-	    'padding-top': "100%"
-	}, "slow", "swing");
-	$("#loading").show();
-	
-	var box = $("#textbox").val();
-	var username = box;
-	var findid = "none";
-	
-	if (box.match(/([SM])(\d+)A(\d+)D(\d+)$/)) {
-	    box = box.match(/([SM])(\d+)A(\d+)D(\d+)$/)[0];
-	    username = box.match(/([SM])(\d+)A(\d+)D(\d+)$/)[2];
-	    findid = box.match(/([SM])(\d+)A(\d+)D(\d+)$/)[3];
-	} else if (box.match(/(profiles\/)(\w+)/) || !isNaN(box)) {
-	    box = box.match(/(profiles\/)(\w+)/) ? box.match(/(profiles\/)(\w+)/)[2] : box;
-	    username = box;
-	} else if (box.match(/(partner.)(\w+)/)) {
-	    box = "7656" + (parseInt(box.match(/(partner.)(\w+)/)[2]) + 1197960265728);
-	    username = box;
-	} else {
-	    var vanity;
-	    if (box.match(/(id\/)(\w+)/)) {
-		box = box.match(/(id\/)(\w+)/)[2];
-		username = box;
-	    }
-	}
-	
-	var requestData = {input: box, id: username, itemid: findid};
-	
-	document.getElementById('textbox').value = box;
-	location.hash = box;
-	
-	function display(data, loadTime) {
-	    $("#container").animate({
-		'padding-top': 5,
-	    }, "slow", "swing");
-	    
-	    $("#display").animate({
-		'padding-top': "0px"
-	    }, "slow", "swing");
-	    
-	    $("#loading").hide();
-	    document.getElementById('display').innerHTML = data;
-	    
-	    $("#display").append("<div style=\"text-align:center;\">Loaded in " + loadTime + " seconds</div><br><br>");
-	}
-	
-	var start = performance.now();
-	$.post("/displayInventory", requestData).done(function (data) {
-	    display(data, ((performance.now()- start)/1000).toFixed(2));
-	});
+        $(this).blur();
+
+        $("#display").animate({
+            'padding-top': "100%"
+        }, "slow", "swing");
+        $("#loading").show();
+
+        var box = $("#textbox").val();
+        var match = box.match(/([SM])(\d+)A(\d+)D(\d+)$/);
+        box = match[0];
+        type = match[1];
+        var requestData = {
+            s: type === 'S' ? match[2] : '0',
+            a: match[3],
+            d: match[4],
+            m: type === 'S' ? '0' : match[2]
+        }
+
+        document.getElementById('textbox').value = box;
+        location.hash = box;
+
+        function display(data, loadTime) {
+            $("#container").animate({
+                'padding-top': 5,
+            }, "slow", "swing");
+
+            $("#display").animate({
+                'padding-top': "0px"
+            }, "slow", "swing");
+
+            $("#loading").hide();
+            document.getElementById('display').innerHTML = data;
+
+            $("#display").append("<div style=\"text-align:center;\">Loaded in " + loadTime + " seconds</div><br><br>");
+        }
+
+        var start = performance.now();
+        $.post("/displayInventory", requestData).done(function (data) {
+            display(data, ((performance.now()- start)/1000).toFixed(2));
+        });
     })
-    
+
     if (window.location.hash) {
-	var hashURL = window.location.hash.substring(1);
-	document.getElementById('textbox').value = hashURL;
-	$("#button")[0].click();
+        var hashURL = window.location.hash.substring(1);
+        document.getElementById('textbox').value = hashURL;
+        $("#button")[0].click();
     }
 });
