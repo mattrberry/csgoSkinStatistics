e2b6e6025296499e2e8397c52a44b5fd5f5b4614
Matthew Berry <me@mattrb.com>
2017-06-12 13:25:42 -0400

    first start to a fix after valve broke the old api

--- inventory.py
+++ inventory.py
@@ -6,8 +6,40 @@ import time
 from skinData import fades, order, doppler
 import os
 
-apikey = os.environ['steam_api_key']
+from steam import SteamClient
+from csgo import CSGOClient
+from csgo.enums import ECsgoGCMsg
 
+import struct
+
+client = SteamClient()
+cs = CSGOClient(client)
+
+@client.on('logged_on')
+def start_csgo():
+    print('launching csgo...')
+    cs.launch()
+
+@cs.on('ready')
+def gc_ready():
+    print('launched csgo')
+    app.run()
+    pass
+
+def send(s, a, d, m):
+    cs.send(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockRequest, {
+        'param_s': s,
+        'param_a': a,
+        'param_d': d,
+        'param_m': m,
+        })
+
+    resp = cs.wait_event(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockResponse, timeout = 10)
+
+    try:
+        return struct.unpack('f', struct.pack('i', resp[0].iteminfo.paintwear))
+    except:
+        return None
 
 app = Flask(__name__)
 
@@ -19,47 +51,28 @@ def home():
 
 @app.route('/displayInventory', methods=["POST"])
 def displayInventory():
-    userinput = request.form['input']
-    steamid = request.form['id']
-    inspectid = request.form['itemid']
+    s = int(request.form['s'])
+    a = int(request.form['a'])
+    d = int(request.form['d'])
+    m = int(request.form['m'])
 
-    try:
-        response = main(convertID(str(steamid)), str(inspectid))
-    except KeyError:
-        response = ("<p style=\"text-align:center\">" +
-                    """This Steam ID appears not to exist or it is private.<br>
-                    If you're absolutely sure that this is incorrect, try again
-                    in a few seconds.<br>If the error persists, use the
-                    \"Contact me.\" button below.<br>""" + "</p>")
+    print('s:{}, a:{}, d:{}, m:{}'.format(s, a, d, m))
 
-    return response
+    resp = None
+    count = 0
+    while resp is None and count < 2:
+        print('count:' + str(count))
+        resp = send(s, a, d, m)
+        count += 1
 
+    if resp is None:
+        return 'Either Steam is slow or that\'s an invalid link'
 
-def convertID(steamid):
-    if not steamid.isdigit():
-        url = ("http://api.steampowered.com/ISteamUser/ResolveVanityURL/" +
-               "v0001/?key=" + apikey + "&vanityurl=" + steamid)
-        response = requests.get(url)
-        steamid = json.loads(response.text)['response']['steamid']
-    return steamid
+    return str(resp[0])
 
 
 def main(steamid, inspectid):
     timeStart = time.time()
-
-    url = ("http://api.steampowered.com/IEconItems_730/GetPlayerItems/" +
-           "v0001/?key=" + apikey + "&SteamID=" + steamid)
-
-    steamAPIcalls = 0
-    while True:
-        response = requests.get(url)
-        if response.text != "{\n\n}":
-            break
-        elif steamAPIcalls >= 50:
-            return "Steam API appears to be slow."
-        steamAPIcalls += 1
-
-    playerItems = json.loads(response.text)
 
     with open('json/itemDB.json') as f:
         itemDB = json.load(f, encoding='utf-8')
@@ -70,132 +83,7 @@ def main(steamid, inspectid):
     with open('json/patternDB.json') as f:
         patternDB = json.load(f, encoding='utf-8')
 
-    skins = []
-    inspect = []
-    keys = {}
-
-    for item in playerItems["result"]["items"]:
-        itemID = item["id"]
-        itemType = item["defindex"]
-        try:
-            itemName = itemDB["item"][str(itemType)]
-        except KeyError:
-            itemName = "graffiti or other untracked item - hopefully"  # pass
-
-        if ((itemType <= 516 or itemType in range(5027, 5035)) and
-                item["attributes"][0]["defindex"] == 6):
-            patternIndex = item["attributes"][0]["float_value"]
-            pattern = skinDB["skin"][str(patternIndex)]
-            paintIndex = str(int(item["attributes"][1]["float_value"]))
-            floatValue = item["attributes"][2]["float_value"]
-
-            special = ""
-
-            if pattern == "Marble Fade":
-                try:
-                    special = patternDB[itemName][pattern][paintIndex]
-                except KeyError:
-                    pass
-            elif pattern == "Fade" and itemName in fades:
-                info = fades[itemName]
-                unscaled = order[::info[1]].index(int(paintIndex))
-                scaled = unscaled / 1001
-                percentage = round(info[0] + scaled * (100 - info[0]))
-                special = str(percentage) + "%"
-            elif pattern == "Doppler" or pattern == "Gamma Doppler":
-                special = doppler[patternIndex]
-
-            for attribute in item["attributes"]:
-                if attribute["defindex"] == 81:
-                    itemName = ("<span class=\"pop\">StatTrak</span> " +
-                                itemName)
-                    break
-
-            if itemType >= 500:
-                itemName = ("<span class=\"pop\">" + u'\u2605 ' +
-                            "</span>" + itemName)
-
-            skin = [itemName, pattern, special, floatValue]
-            skins.append(skin)
-
-            if str(itemID) == inspectid:
-                inspect.append(skin)
-
-        elif "Key" in itemName:
-            if itemName in keys:
-                keys[itemName] += 1
-            else:
-                keys[itemName] = 1
-
-    if inspectid != "none" and len(inspect) < 1:
-        inspect.append(["No longer in this inventory.", "", "", -1])
-
-    return ("<table>" + inspectString(inspect) + skinString(skins) +
-            keyString(keys) + "</table>")
-
-
-def inspectString(inspect):
-    try:
-        inspectString = "<tr><th>Inspected Item</th><th>Float</th></tr>"
-        if inspect[0][3] > 0:
-            inspectString += outputWeapon(inspect)
-        else:
-            inspectString += "<tr><td>" + inspect[0][0] + "</td><td></td></tr>"
-    except:
-        inspectString = ""
-
-    return inspectString
-
-
-def skinString(skins):
-    skinString = ""
-
-    if len(skins) != 0:
-        skinString += "<tr><th>Skins</th>"
-        skinString += ("<th>" + str(len(skins)) + "</th></tr>")
-        skinString += outputWeapon(skins)
-
-    return skinString
-
-
-def outputWeapon(weapons):
-    weapons = sortFloat(weapons)
-    weaponString = ""
-
-    for weapon in weapons:
-        weaponString += ("<tr><td>" + weapon[0] + " | " + weapon[1] +
-                         "<sup><span style=\"font-size:12px\" class=pop> " +
-                         weapon[2] + "</span></sup></td>" +
-                         "<td>" + "{0:.12f}".format(weapon[3]) + "</td></tr>")
-
-    return weaponString
-
-
-def sortFloat(skns):
-    floats = [skn[3] for skn in skns]
-    return [s for (f, s) in sorted(zip(floats, skns))]
-
-
-def keyString(keys):
-    keyList = sortKeys(keys)
-    keyString = ""
-
-    if len(keyList) != 0:
-        keyString += ("<tr><th>Keys</th><th>" +
-                      str(sum([keys[k] for k in keys])) +
-                      "</th></tr>")
-
-        for key in keyList:
-            keyString += ("<tr><td>" + key[0] + "</td>" +
-                          "<td>" + str(key[1]) + "</td></tr>")
-
-    return keyString
-
-
-def sortKeys(keys):
-    keyList = [[key, keys[key]] for key in keys]
-    counts = [c for (n, c) in keyList]
-    return [k for (c, k) in sorted(zip(counts, keyList))[::-1]]
 
 if __name__ == '__main__':
-    app.run()
+    client.login(os.environ['steam_user'], os.environ['steam_pass'])
+    client.run_forever()
