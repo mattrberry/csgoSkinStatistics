82d57c4b14674bb37f0aa960d8ac3b1b8786d0a2
Matthew Berry <me@mattrb.com>
2017-06-19 19:57:01 -0400

    no longer posts non-inspect links

--- static/js/post.js
+++ static/js/post.js
@@ -1,3 +1,18 @@
+function display(data, loadTime) {
+    $("#container").animate({
+        'padding-top': 5,
+    }, "slow", "swing");
+
+    $("#display").animate({
+        'padding-top': "0px"
+    }, "slow", "swing");
+
+    $("#loading").hide();
+    document.getElementById('display').innerHTML = data;
+
+    $("#display").append("<div style=\"text-align:center;\">Loaded in " + loadTime + " seconds</div><br><br>");
+}
+
 $(document).ready(function() {
     $("#textbox").keydown(function(e){
         if(e.which === 13){
@@ -14,38 +29,28 @@ $(document).ready(function() {
         $("#loading").show();
 
         var box = $("#textbox").val();
-        var match = box.match(/([SM])(\d+)A(\d+)D(\d+)$/);
-        box = match[0];
-        type = match[1];
-        var requestData = {
-            s: type === 'S' ? match[2] : '0',
-            a: match[3],
-            d: match[4],
-            m: type === 'S' ? '0' : match[2]
+
+        try {
+            var match = box.match(/([SM])(\d+)A(\d+)D(\d+)$/);
+            box = match[0];
+            type = match[1];
+            var requestData = {
+                s: type === 'S' ? match[2] : '0',
+                a: match[3],
+                d: match[4],
+                m: type === 'S' ? '0' : match[2]
+            }
+
+            document.getElementById('textbox').value = box;
+            location.hash = box;
+
+            var start = performance.now();
+            $.post("/displayInventory", requestData).done(function (data) {
+                display(data, ((performance.now()- start)/1000).toFixed(2));
+            });
+        } catch (e) {
+            display("<p>Not a valid inspect link</p>", "0.0")
         }
-
-        document.getElementById('textbox').value = box;
-        location.hash = box;
-
-        function display(data, loadTime) {
-            $("#container").animate({
-                'padding-top': 5,
-            }, "slow", "swing");
-
-            $("#display").animate({
-                'padding-top': "0px"
-            }, "slow", "swing");
-
-            $("#loading").hide();
-            document.getElementById('display').innerHTML = data;
-
-            $("#display").append("<div style=\"text-align:center;\">Loaded in " + loadTime + " seconds</div><br><br>");
-        }
-
-        var start = performance.now();
-        $.post("/displayInventory", requestData).done(function (data) {
-            display(data, ((performance.now()- start)/1000).toFixed(2));
-        });
     })
 
     if (window.location.hash) {
