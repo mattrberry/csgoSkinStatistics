efaf45566ff1686e802b5d8656c06a377494ce56
Matthew Berry <me@mattrb.com>
2017-06-20 01:46:38 -0400

    returns all iteminfo to javascript now

--- skinstats.py
+++ skinstats.py
@@ -6,7 +6,7 @@ from csgo_worker import CSGOWorker
 
 import logging
 logging.basicConfig(format="%(asctime)s | %(name)s | %(message)s", level=logging.INFO)
-LOG = logging.getLogger('CSGO GC API')
+LOG = logging.getLogger('csgo gc api')
 
 app = Flask('CSGO GC API')
 
@@ -23,35 +23,29 @@ def displayInventory():
     d = int(request.form['d'])
     m = int(request.form['m'])
     
-    paintwear = None
+    try:
+        iteminfo = worker.send(s, a, d, m)
+    except TypeError:
+        return 'Invalid link or Steam is slow.'
 
-    for i in range(1):
-        try:
-            paintwear = worker.send(s, a, d, m)
-            break
-        except:
-            pass
+    LOG.info('returning: {}'.format(str(iteminfo)))
 
-    if not paintwear:
-        paintwear = 'Invalid link or Steam is slow.'
- 
-    return paintwear
-
+    return str(iteminfo)
 
 if __name__ == '__main__':
-    LOG.info('Simple CSGO GC')
-    LOG.info('---------------')
-    LOG.info('Starting worker')
+    LOG.info('simple csgo gc')
+    LOG.info('--------------')
+    LOG.info('starting worker')
 
     worker = CSGOWorker()
 
     worker.start()
 
-    LOG.info('Starting server')
+    LOG.info('starting server')
     http_server = WSGIServer(('', 5000), app)
 
     try:
         http_server.serve_forever()
     except:
-        LOG.info('Exit requested')
+        LOG.info('exit requested')
         worker.close()
